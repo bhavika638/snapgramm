8d41feb9eff045429051981a28c435dca81f0564
bhavika <bhavikakatyal35@gmail.com>
2024-10-11 22:25:41 +0530

    changes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+// import './globla.css'
+
+const App = () => {
+  return (
+    
+      <h1 className="text-3xl font-black text-underline">Hello Snapgram</h1>
+  )
+}
+
+export default App
